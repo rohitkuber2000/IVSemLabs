875389701bf15ac3218c8195dd1eb7955d41d117
rohitkuber2000 <46227008+rohitkuber2000@users.noreply.github.com>
2020-01-18 16:56:32 +0530

    Update employee.cpp

--- 180953124/employee.cpp
+++ 180953124/employee.cpp
@@ -39,6 +39,48 @@ i->next = temp;
 
 }
 }
+void sll::sortl(sll* first){
+
+    sll* k = new sll() ;
+    for(sll* i= first;i!=NULL;i=i->next){
+        for(sll* j = i->next;j!=NULL;j=j->next){
+            if(i->Salary>j->Salary){
+                strcpy(k->Name,i->Name);
+                k->Age = i->Age;
+               strcpy(k->Department,i->Department);
+                k->Salary = i ->Salary;
+                strcpy(i->Name,j->Name);
+                 i->Age = j->Age;
+                strcpy(i->Department,j->Department);
+                 i->Salary = j->Salary;
+                strcpy(j->Name,k->Name);
+                 j->Age = k->Age;
+                strcpy(j->Department,k->Department);
+                 j->Salary = k->Salary;
+//                swapq(&i,&j);
+            }
+        }
+}
+}
+void sll::displayl(sll* first){
+    if(first==NULL){
+        cout<<"The list is empty";
+    }
+    else{
+        sll* i = first;
+        while(i->next!=NULL){
+            cout<<i->Name<<endl;
+            cout<<i->Age<<endl;
+            cout<<i->Department<<endl;
+            cout<<i->Salary<<endl;
+            i=i->next;
+        }
+        cout<<i->Name<<endl;
+        cout<<i->Age<<endl;
+        cout<<i->Department<<endl;
+        cout<<i->Salary<<endl;
+    }
+}
 
 
 
@@ -61,6 +103,12 @@ cout<<"\n Enter employee"<<i+1<<"Salary:";
 cin>>sal;
 s.insertq(head,na,ag,dep,sal);
 }
-
+cout<<"Displaying the details of the employees";
+s.displayl(head);
+    
+cout<<"Sorting on the basis of salary"<<endl;
+    s.sortl(head);
+cout<<"Displaying the details of the employees";
+s.displayl(head);
 return 0;
 }

cc2e13198d97ff086945f76303e277f5921b525f
rohitkuber2000 <46227008+rohitkuber2000@users.noreply.github.com>
2020-01-18 17:03:29 +0530

    Update insertion.cpp

--- 180953124/insertion.cpp
+++ 180953124/insertion.cpp
@@ -19,6 +19,21 @@ a[j+1] = item;
 }
 }
 
+void recinsertion(int a[],int n){
+  if(n<=1){return ;
+          }
+  
+  recinsertion(a,n-1);
+  int last = a[n-1];
+  int j=n-2;
+  
+  while(j>=0&&a[j]>last){
+    a[j+1] = a[j];
+    j = j-1;
+  }
+  a[j+1] = last;
+}
+
 int main(){
 int a[] = {9,8,7,6,5};
 
